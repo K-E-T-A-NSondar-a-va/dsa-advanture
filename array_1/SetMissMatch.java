61517f4d8e2591d770c991c084fd4b5180d072d4
ketan <ketansondarva2@gmail.com>
2026-01-01 22:24:28 +0530

    other two problams

--- array_1/SetMissMatch.java
+++ array_1/SetMissMatch.java
@@ -1,5 +1,7 @@
 package array_1;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -23,4 +25,62 @@ public class SetMissMatch {
         ans[1] = sec;
         return ans;
     }
+
+        static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (FileWriter writer = new FileWriter("display_runtime.txt")) {
+                writer.write("0");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }));
+    }
+    
+    // optimized by you: in single loop:
+    // public int[] findErrorNums(int[] nums) {
+    //    int ans[] = new int[2];
+    //    Set<Integer> set = new HashSet<>();
+       
+    //    int n = nums.length;
+
+    //    int sum = 0;
+    //    for(int num : nums) {
+    //     if(!set.add(num)) {
+    //         ans[0] = num;
+    //     }
+    //     sum += num;
+    //    }
+
+    //    int total = (n * (n + 1)) / 2;
+    //    System.out.println(total);
+    //    if(total > sum)
+    //      ans[1] = (total - sum) + ans[0];
+    //    else
+    //      ans[1] = ans[0] - (sum - total);
+
+    //    return ans;
+    // }
+
+    // If the duplicate consicutive, this would have workd as well (without set) (O(n)):
+    // public int[] findErrorNums(int[] nums) {
+    //    int ans[] = new int[2];
+
+    //    int n = nums.length;
+    //    int sum = nums[0];
+    //    for(int i = 1; i < n; i++) {
+    //     if(nums[i-1] == nums[i]) {
+    //         ans[0] = nums[i];
+    //     }
+    //     sum += nums[i];
+    //    }
+
+    //    int total = (n * (n + 1)) / 2;
+    //    System.out.println(total);
+    //    if(total > sum)
+    //      ans[1] = (total - sum) + ans[0];
+    //    else
+    //      ans[1] = ans[0] - (sum - total);
+
+    //    return ans;
+    // }
 }
---
+++ array_1/SmallerNumThenCurrent.java
@@ -0,0 +1,21 @@
+package array_1;
+
+public class SmallerNumThenCurrent {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int n = nums.length;
+
+        int ans[] = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++){
+                if(i != j) {
+                    if(nums[i] > nums[j]) {
+                        ans[i]++;
+                    } 
+                }
+            }
+        }
+
+        return ans;
+    }
+}
